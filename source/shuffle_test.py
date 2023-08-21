7a1c3e7a7963fcace02c46830a660996a0530789
keru <45701634+kerufu@users.noreply.github.com>
2023-08-20 20:53:32 -0700

    Update shuffle_test.py

--- source/shuffle_test.py
+++ source/shuffle_test.py
@@ -5,12 +5,12 @@ def printDs(ds):
     for example in ds.take(ds.cardinality()):
         print(example)
 
-data = list(range(500))
+data = list(range(5000))
 ds = tf.data.Dataset.from_tensor_slices(data)
 
-ds = ds.shuffle(150, reshuffle_each_iteration=True)
+ds = ds.shuffle(5000//3, reshuffle_each_iteration=True)
 
-ds = ds.batch(10)
+ds = ds.batch(64)
 
 ds = ds.shuffle(10, reshuffle_each_iteration=True)
 
