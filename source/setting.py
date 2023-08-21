48e13bc344932586c1210f5656c946deb29f0549
keru <45701634+kerufu@users.noreply.github.com>
2023-08-20 21:34:41 -0700

    Update setting.py

--- source/setting.py
+++ source/setting.py
@@ -29,8 +29,8 @@ wgan_generator_path = "saved_model/wgan_generator"
 wgan_discriminator_path = "saved_model/wgan_discriminator"
 
 
-caae_discriminator_weight = 0.5
-wgan_discriminator_weight = 0.5
+caae_discriminator_weight = 0.2
+wgan_discriminator_weight = 0.8
 gradient_penalty_weight = 1
 
 batch_size = 64
